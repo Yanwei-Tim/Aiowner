0cb9ee47795a6f1d1547a746c00771f6bc91a618
dengjun <jun.deng@dudu.tm>
2016-02-01 11:33:52 +0800

    主页UI设计
    Change-Id: I6f006dde1a20a16c8d1d2d247bf782b43e9253be

--- Aiowner/app/src/main/java/com/dudu/aiowner/ui/main/MainActivity.java
+++ Aiowner/app/src/main/java/com/dudu/aiowner/ui/main/MainActivity.java
@@ -1,38 +1,54 @@
 package com.dudu.aiowner.ui.main;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.database.DatabaseUtils;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.view.View;
 
 import com.dudu.aiowner.R;
+import com.dudu.aiowner.databinding.ActivityMainBinding;
+import com.dudu.aiowner.ui.activity.preventTheft.PreventTheftActivity;
+import com.dudu.aiowner.ui.main.observable.MainObservable;
+import com.dudu.aiowner.utils.ActivitiesManager;
 
 /**
  * Created by sunny_zhang on 2016/1/27.
  */
 public class MainActivity extends Activity {
+    private MainObservable mainObservable;
+    private ActivityMainBinding activityMainBinding;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivitiesManager.getInstance().addActivity(this);
+
+        mainObservable = new MainObservable();
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        activityMainBinding.setMainPage(mainObservable);
 
     }
 
-    public void startPreventTheft() {
-
+    public void startPreventTheft(View view) {
+        startActivity(new Intent(MainActivity.this, PreventTheftActivity.class));
     }
 
-    public void startPreventLooting() {
-
+    public void startPreventLooting(View view) {
+        startActivity(new Intent(MainActivity.this, PreventTheftActivity.class));
     }
 
-    public void startTestSpeed() {
-
+    public void startTestSpeed(View view) {
+        startActivity(new Intent(MainActivity.this, PreventTheftActivity.class));
     }
 
-    public void startDrive() {
-
+    public void startDrive(View view) {
+        startActivity(new Intent(MainActivity.this, PreventTheftActivity.class));
     }
 
-    public void startMaintenanceAssistant() {
-
+    public void startMaintenanceAssistant(View view) {
+        startActivity(new Intent(MainActivity.this, PreventTheftActivity.class));
     }
 }
