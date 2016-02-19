288f37045b8374eb3b69a8c589ccc1693bd57172
sunny.zhang <sunny.zhang@dudu.tm>
2016-02-19 19:42:10 +0800

    添加主页车底部图片
    Change-Id: I32bca32a279d34e4c70d4a13e6fe993f2810fcfe

---
+++ Aiowner/app/src/main/java/com/dudu/aiowner/ui/activity/testSpeed/AccImagesList.java
@@ -0,0 +1,56 @@
+package com.dudu.aiowner.ui.activity.testSpeed;
+
+import com.dudu.aiowner.R;
+
+/**
+ * Created by Administrator on 2016/2/18.
+ */
+public class AccImagesList {
+    private int[] images;
+
+    public int[] getImages() {
+        images = new int[41];
+        images[0] = R.drawable.acc01;
+        images[1] = R.drawable.acc02;
+        images[2] = R.drawable.acc03;
+        images[3] = R.drawable.acc04;
+        images[4] = R.drawable.acc05;
+        images[5] = R.drawable.acc06;
+        images[6] = R.drawable.acc07;
+        images[7] = R.drawable.acc08;
+        images[8] = R.drawable.acc09;
+        images[9] = R.drawable.acc10;
+        images[10] = R.drawable.acc11;
+        images[11] = R.drawable.acc12;
+        images[12] = R.drawable.acc13;
+        images[13] = R.drawable.acc14;
+        images[14] = R.drawable.acc15;
+        images[15] = R.drawable.acc16;
+        images[16] = R.drawable.acc17;
+        images[17] = R.drawable.acc18;
+        images[18] = R.drawable.acc19;
+        images[19] = R.drawable.acc20;
+        images[20] = R.drawable.acc21;
+        images[21] = R.drawable.acc22;
+        images[22] = R.drawable.acc23;
+        images[23] = R.drawable.acc24;
+        images[24] = R.drawable.acc25;
+        images[25] = R.drawable.acc26;
+        images[26] = R.drawable.acc27;
+        images[27] = R.drawable.acc28;
+        images[28] = R.drawable.acc29;
+        images[29] = R.drawable.acc30;
+        images[30] = R.drawable.acc31;
+        images[31] = R.drawable.acc32;
+        images[32] = R.drawable.acc33;
+        images[33] = R.drawable.acc34;
+        images[34] = R.drawable.acc35;
+        images[35] = R.drawable.acc36;
+        images[36] = R.drawable.acc37;
+        images[37] = R.drawable.acc38;
+        images[38] = R.drawable.acc39;
+        images[39] = R.drawable.acc40;
+        images[40] = R.drawable.acc41;
+        return images;
+    }
+}
---
+++ Aiowner/app/src/main/java/com/dudu/aiowner/ui/main/CarAnimation.java
@@ -0,0 +1,53 @@
+package com.dudu.aiowner.ui.main;
+
+/**
+ * Created by Administrator on 2015/12/11.
+ */
+
+import android.content.Context;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+
+public class CarAnimation {
+    private ImageView mImageView;
+    private int[] mFrameRess;
+    private int mDuration;
+    private FrameLayout login_layout;
+
+    private int mLastFrameNo;
+    private Context mContext;
+
+    public  void play(ImageView pImageView, int[] pFrameRess, int pDuration, Context mContext, FrameLayout login_layout) {
+        mImageView = pImageView;
+        mFrameRess = pFrameRess;
+        mDuration = pDuration;
+        mLastFrameNo = pFrameRess.length - 1;
+        this.mContext = mContext;
+        this.login_layout = login_layout;
+        mImageView.setBackgroundResource(mFrameRess[0]);
+        playConstant(1);
+    }
+
+    private void playConstant(final int pFrameNo) {
+        mImageView.postDelayed(new Runnable() {
+            public void run() {
+                mImageView.setBackgroundResource(mFrameRess[pFrameNo]);
+                if (pFrameNo == mLastFrameNo) {
+
+                    //设置循环播放
+                    playConstant(0);
+
+                    ImageView finalImage = new ImageView(mContext);
+                    finalImage.setAlpha(10);
+
+                    /*通知imageView 刷新屏幕*/
+                    finalImage.postInvalidate();
+                    return;
+                } else {
+                    playConstant(pFrameNo + 1);
+                }
+            }
+        }, 50);
+    }
+}
