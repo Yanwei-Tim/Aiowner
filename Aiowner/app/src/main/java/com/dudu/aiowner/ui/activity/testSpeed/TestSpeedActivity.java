a45ca478158cdb0be26c605c0c3ce7c76aad2927
sunny.zhang <sunny.zhang@dudu.tm>
2016-02-22 02:28:01 +0800

    添加加速数字动画及加速结果获取
    Change-Id: Ie837484fb301e03bad5b5574a7c765bbb543e300

--- Aiowner/app/src/main/java/com/dudu/aiowner/ui/activity/testSpeed/TestSpeedActivity.java
+++ Aiowner/app/src/main/java/com/dudu/aiowner/ui/activity/testSpeed/TestSpeedActivity.java
@@ -3,6 +3,7 @@ package com.dudu.aiowner.ui.activity.testSpeed;
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -14,12 +15,15 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.dudu.aiowner.R;
+import com.dudu.aiowner.commonlib.commonutils.RiseNumberTextView;
 import com.dudu.aiowner.ui.activity.user.UserInfoActivity;
 import com.dudu.aiowner.ui.base.BaseActivity;
 import com.dudu.aiowner.ui.base.animation.AnimationView;
-import com.dudu.workflow.common.RequestFactory;
 import com.dudu.workflow.common.ObservableFactory;
+import com.dudu.workflow.common.RequestFactory;
 import com.dudu.workflow.driving.DrivingRequest;
+
+import java.util.Random;
 
 /**
  * Created by Administrator on 2016/2/2.
@@ -38,6 +42,7 @@ public class TestSpeedActivity extends BaseActivity {
     private RadioButton acc_200km;
     private RadioButton acc_300km;
     private AnimationDrawable animationDrawable;
+    private RiseNumberTextView acc_result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,16 +50,6 @@ public class TestSpeedActivity extends BaseActivity {
 
         initView();
         initEvent();
-
-//        /*android帧动画animation-list*/
-//        images = new AccImagesList().getImages();
-//        new TestingAnimation().play(mImageView, images, 5, this, count_down);
-
-////
-//        ani_rl = (RelativeLayout) findViewById(R.id.check);
-//        animationView = new AnimationView(this, "acctesting", 1, 41);
-//        ani_rl.addView(animationView);
-
     }
 
     private void initEvent() {
@@ -73,6 +68,9 @@ public class TestSpeedActivity extends BaseActivity {
         acc_100km = (RadioButton) findViewById(R.id.acc_texting_100km_rb);
         acc_200km = (RadioButton) findViewById(R.id.acc_texting_200km_rb);
         acc_300km = (RadioButton) findViewById(R.id.acc_texting_300km_rb);
+
+        // 获取到RiseNumberTextView对象
+        acc_result = (RiseNumberTextView) findViewById(R.id.acc_testing_result_tv);
     }
 
     @Override
@@ -87,32 +85,12 @@ public class TestSpeedActivity extends BaseActivity {
             //false为动画循环播放
             animationDrawable.setOneShot(false);
             animationDrawable.start();
+
+            //为了演示用，如果能获取到加速结果数据，则不用这句
+            setupData(new Random().nextFloat());
+
         }
     }
-
-//        if (acc_100km.isChecked() || acc_200km.isChecked() || acc_300km.isChecked()) {
-//
-//            /**启动加速环动画*/
-//            animationView = new AnimationView(this, "acctesting", 1, 41);
-//            ani_rl.addView(animationView);
-//
-//        } else {
-//            Toast.makeText(getApplicationContext(), "请选择公里数", Toast.LENGTH_SHORT).show();
-//        }
-
-//        int radioButtonId = acc_texting_radiogroup.getCheckedRadioButtonId();
-//        switch (radioButtonId) {
-//            case R.id.acc_texting_100km_rb:
-//                //TODO
-//                break;
-//            case R.id.acc_texting_200km_rb:
-//                //TODO
-//                break;
-//            case R.id.acc_texting_300km_rb:
-//                //TODO
-//                break;
-//        }
-//}
 
     public void userInfo(View view) {
         startActivity(new Intent(TestSpeedActivity.this, UserInfoActivity.class));
@@ -125,7 +103,19 @@ public class TestSpeedActivity extends BaseActivity {
     @Override
     protected void onResume() {
         ObservableFactory.getAccTestFlow()
-                .subscribe();
+                .subscribe(accTestData -> {
+
+                    //获取加速测试结果
+                    Log.v("acc", "accTestData.getAccTotalTime()" + accTestData.getAccTotalTime());
+
+                    setupData(Float.valueOf(accTestData.getAccTotalTime()));
+
+                    //加速结果单位
+                    accTestData.getAccType();
+
+                    //加速结果时间
+                    accTestData.getDateTime();
+                });
         observableFactory.getTitleObservable().titleText.set("加速测试");
         observableFactory.getTitleObservable().userIcon.set(true);
         observableFactory.getCommonObservable().hasBottomIcon.set(false);
@@ -159,4 +149,20 @@ public class TestSpeedActivity extends BaseActivity {
             }
         }
     };
+
+    private void setupData(float resultData) {
+
+        // 设置数据
+        acc_result.withNumber(resultData);
+        // 设置动画播放时间
+        acc_result.setDuration(5000);
+        acc_result.start();
+
+        acc_result.setOnEndListener(new RiseNumberTextView.EndListener() {
+            @Override
+            public void onEndFinish() {
+                animationDrawable.stop();
+            }
+        });
+    }
 }
